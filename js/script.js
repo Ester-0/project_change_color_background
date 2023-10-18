dd36d3031245320940b1922f91e1243fab6983d0
Ester-0 <azalea_@hotmail.com>
2023-10-18 16:58:02 +0200

    No funciona

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 //En este array de strings, tenemos todos los colores
 const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
 
+// SIN QUE SEA ALEATORIO
+// function changeColor() {
+//     document.querySelector("body").style.backgroundColor = colors[0];
+// }
 
+//AYUDA para que sea aleatorio: https://www.javatpoint.com/how-to-pick-random-elements-from-an-array
+
+
+function changeColor() {
+     
+    document.querySelector("body").style.backgroundColor = colors[Math.floor(Math.random() * colors.legth)];
+        
+           
+    }
